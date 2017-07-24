1cd0e9f1a449e6714bdd6bf2b7a5688614aa418c
Nikolay <n.poymanov@gmail.com>
2017-07-24 20:45:23 +0300

    Add arrow icons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.info-block-header').on('click', function () {
+       $(this).toggleClass('info-block-header-active');
        $(this).next('.info-block-content').slideToggle('400');
     });
 });
